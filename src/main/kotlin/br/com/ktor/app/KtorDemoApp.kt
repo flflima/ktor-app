6168da383dea93e8dbf6f0e6e69ce81d44dac79c
Felipe Lima <flf.felipe@gmail.com>
2019-09-08 17:49:01 -0300

    Add jackson configuration

--- src/main/kotlin/br/com/ktor/app/KtorDemoApp.kt
+++ src/main/kotlin/br/com/ktor/app/KtorDemoApp.kt
@@ -1,11 +1,15 @@
 package br.com.ktor.app
 
 import br.com.ktor.app.model.Person
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
+import io.ktor.jackson.jackson
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.response.respondText
@@ -17,6 +21,13 @@ import io.ktor.server.netty.Netty
 import java.lang.System.getenv
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        jackson {
+            configure(SerializationFeature.INDENT_OUTPUT, true)
+            configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            registerModule(JavaTimeModule())
+        }
+    }
     install(DefaultHeaders)
     install(CallLogging)
     install(Routing) {
@@ -28,7 +39,7 @@ fun Application.module() {
         }
         post("/persons") {
             val person = call.receive<Person>()
-            call.respond(mapOf("OK" to true))
+            call.respond(mapOf("person" to person))
         }
     }
 }
--- src/main/kotlin/br/com/ktor/app/model/Person.kt
+++ src/main/kotlin/br/com/ktor/app/model/Person.kt
@@ -2,8 +2,4 @@ package br.com.ktor.app.model
 
 import java.time.LocalDate
 
-data class Person (val name: String,
-                   val document: String,
-                   val birthDate: LocalDate,
-                   val address: String,
-                   val email: String)
+data class Person(val name: String, val document: String, val birthdate: LocalDate, val address: String, val email: String)
